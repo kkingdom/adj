a5d5703d5377fde5b252a0b29260fc38f8126bc8
llingdom <minkai.cao@gmail.com>
2023-12-29 14:52:58 +0800

    add index and view

--- ad/views.py
+++ ad/views.py
@@ -3,5 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from ad.models import Question
+
+
 def index(request):
-    return  HttpResponse("Hello, world. You're at the")
+    last_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in last_question_list])
+    return  HttpResponse(output)
+
+def details(request,question_id):
+    return HttpResponse("Your question id is %d" % question_id)
+
+def results(request,question_id):
+    response = "You're looking at question id %s"
+    return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+    return HttpResponse("You're voting on question id %d" % question_id)
