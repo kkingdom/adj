9f00067d719c237f941d7affe1d415bd34d00e81
llingdom <minkai.cao@gmail.com>
2024-01-02 17:21:51 +0800

    chat 5 test

--- polls/tests.py
+++ polls/tests.py
@@ -62,13 +62,13 @@ class QuestionViewTests(TestCase):
 class QuestionDetailViewTest(TestCase):
     def test_future_question(self):
         future_question = create_question(question_text='Future question', days=5)
-        url = reverse('polls:detail', kwargs={'pk':future_question.id})
+        url = reverse('polls:detail', args=future_question.id)
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
     def test_past_question(self):
         past_question = create_question(question_text='Past question', days=-5)
-        url = reverse('polls:detail', kwargs={'pk':past_question.pk})
+        url = reverse('polls:detail', args=past_question.id)
         response = self.client.get(url)
         self.assertContains(response, 'Past question')
 
