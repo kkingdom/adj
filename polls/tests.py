3c1d152bbf23b691f776d953a8e645b7df029885
llingdom <minkai.cao@gmail.com>
2024-01-02 17:12:07 +0800

    chat 5 test

--- polls/tests.py
+++ polls/tests.py
@@ -47,7 +47,7 @@ class QuestionViewTests(TestCase):
         question = create_question(question_text="Past question.", days=-30)
         create_question(question_text="Future question.", days=30)
         response = self.client.get(reverse("polls:index"))
-        self.assertQuerySetEqual(
+        self.assertQuerysetEqual(
             response.context["latest_question_list"],
             [question],
         )
