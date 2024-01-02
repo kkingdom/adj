cad33cadca6f747416228e00ffce5df9726075aa
llingdom <minkai.cao@gmail.com>
2024-01-02 11:39:47 +0800

    chat 5 test

--- polls/tests.py
+++ polls/tests.py
@@ -2,9 +2,51 @@ from django.test import TestCase
 
 # Create your tests here.
 from datetime import datetime, timedelta
+
+from django.urls import reverse
 from django.utils import timezone
 
 from polls.models import Question
+
+
+def create_question(question_text,days):
+    time = timezone.now() + timedelta(days=days)
+    return Question.objects.create(question_text=question_text,pub_date=time)
+
+class QuestionViewTests(TestCase):
+    def test_no_questions(self):
+        response = self.client.get(reverse('polls_index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No polls available')
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_past_question(self):
+        question = create_question(question_text='Past question', days=-30)
+        response = self.client.get(reverse('polls_index'))
+        self.assertQuerysetEqual(
+            response.context['last_question_list'], [question]
+        )
+
+    def test_future_question(self):
+        question = create_question(question_text='Future question', days=30)
+        response = self.client.get(reverse('polls_index'))
+        self.assertQuerysetEqual(
+            response.context['last_question_list'], []
+        )
+
+    def test_future_and_past_question(self):
+        question = create_question(question_text='Future question', days=30)
+        create_question(question_text='Past question', days=-30)
+        response = self.client.get(reverse('polls_index'))
+        self.assertQuerysetEqual(response.context['last_question_list'], [question])
+
+    def test_two_past_questions(self):
+        question1 = create_question(question_text='Past question 1.',days=-30)
+        question2 = create_question(question_text='Past question 2.', days=-5)
+        response = self.client.get(reverse('polls_index'))
+        self.assertQuerysetEqual(response.context['last_question_list'],
+                                 [question2, question1],
+                                 )
 
 
 class QuestionModelTest(TestCase):
@@ -16,7 +58,7 @@ class QuestionModelTest(TestCase):
     def test_was_published_recently_with_old_question(self):
         time = timezone.now() - timedelta(days=1,seconds=1)
         old_question = Question(pub_date=time)
-        self.assertIs(old_question.was_published_recently(),True)
+        self.assertIs(old_question.was_published_recently(),False)
 
     def test_was_published_recently_with_recent_question(self):
         time = timezone.now() - timedelta(hours=23,minutes=59,seconds=59)
